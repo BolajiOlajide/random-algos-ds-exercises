95d7bd9070666b75b5dbcac70ac4246f4734b809
Bolaji Olajide <cooproton@gmail.com>
2021-03-29 10:12:27 +0100

    complete insertion sort with while loop

---
+++ hackerrank/3_insertion_sort_3.py
@@ -0,0 +1,17 @@
+
+
+def insertion_sort(l):
+    for i in range(1, len(l)):
+        j = i-1
+        key = l[i]
+
+        while (j >= 0) and (l[j] > key):
+            l[j+1] = l[j]
+            j -= 1
+        l[j+1] = key
+
+m = '7 4 3 5 8 2'
+ar = [int(i) for i in m.strip().split()]
+
+insertion_sort(ar)
+print(" ".join(map(str,ar)))
